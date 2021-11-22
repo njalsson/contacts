51b27a84cf263f3bd153c0a535f142abf9f7bcfa
aron bergur jóhannsson <aronnemi@gmail.com>
2021-11-22 15:41:54 +0000

    eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 13,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    ],
+    rules: {
+        'react/function-component-definition': [
+            2,
+            {
+                namedComponents: 'function-declaration',
+            },
+        ],
+        indent: [2, 4],
+        'react/jsx-indent': [2, 4],
+    },
+};
