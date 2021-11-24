deec428fd982fd519e33ecec811207862d818791
Daníel Freyr Gylfason <54936265+danielfreyrg@users.noreply.github.com>
2021-11-24 16:33:29 +0000

    eitthvað

--- App.jsx
+++ App.jsx
@@ -1,10 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< Updated upstream
-=======
 import Task from './src/components/Task';
->>>>>>> Stashed changes
 
 const styles = StyleSheet.create({
     container: {
@@ -17,13 +14,9 @@ const styles = StyleSheet.create({
 function App() {
     return (
         <View style={styles.container}>
-<<<<<<< Updated upstream
-            <Text>Open up App.js to start working on your app!</Text>
-=======
             <Text>Open up App.js to start working on your app my arse!
             <Task />
             </Text>
->>>>>>> Stashed changes
             <StatusBar />
         </View>
     );
