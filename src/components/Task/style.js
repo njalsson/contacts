01e050c00988051ec38dff88d2253a4685a3079a
Daníel Freyr Gylfason <54936265+danielfreyrg@users.noreply.github.com>
2021-11-25 22:38:55 +0000

    list css

--- src/components/Task/style.js
+++ src/components/Task/style.js
@@ -3,8 +3,12 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     task: {
         marginTop: 10,
-        borderRadius: 5,
-        backgroundColor: 'lightgrey',
+        overflow: 'hidden',
+        borderRadius: 10,
+        backgroundColor: 'rgba(150, 161, 179, 0.4)',
+        borderColor: 'rgba(48, 56, 55, 0.4)',
+        borderWidth: 1,
+        color: 'black',
         padding: 5,
 
     },
