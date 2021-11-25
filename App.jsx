85d4aa1184094396072aac35aeae8d754efe4582
Daníel Freyr Gylfason <54936265+danielfreyrg@users.noreply.github.com>
2021-11-25 18:00:59 +0000

    can get task by listId

--- App.jsx
+++ App.jsx
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import data from './src/resources/data.json';
 import Boards from './src/components/Boards/Boards';
+import Task from './src/components/Task/Task';
 
 const styles = StyleSheet.create({
     container: {
@@ -17,9 +18,10 @@ export default function App() {
     const [boards, setBoards] = useState(data.boards);
     const [lists, setLists] = useState(data.lists);
     const [tasks, setTasks] = useState(data.tasks);
-    console.log(boards);
+    // console.log(tasks);
     return (
         <View style={styles.container}>
+            <Task listId={8} taskdata={tasks} />
             <Boards boards={boards} setBoards={setBoards} />
             <StatusBar />
         </View>
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,17 +3,21 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable react/jsx-wrap-multilines */
 /* eslint-disable react/jsx-filename-extension */
+
 import React from 'react';
 import { FlatList, Text } from 'react-native';
-import { tasks } from './data.json';
+// import { tasks } from './data.json';
 import style from './style';
 
-function Task() {
+function Task(listId, taskdata) {
+  console.log(listId.taskdata);
+  const test2 = listId.taskdata.filter((task) => task.listId === listId.listId);
+  console.log('Data', test2);
     return (
         <FlatList
-          data={tasks}
-          keyExtractor={({ id }) => id}
-          renderItem={({ item }) => (
+            data={listId.taskdata.filter((task) => task.listId === listId.listId)}
+            keyExtractor={({ id }) => id}
+            renderItem={({ item }) => (
             <Text style={style.task}>{`${item.id}. ${item.name} \n \t ${item.description}`}</Text>
           )}
         />
