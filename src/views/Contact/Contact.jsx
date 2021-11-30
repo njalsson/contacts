dfba469e1112e8a981b09e94c9f3b3407a7fc676
Daníel Freyr Gylfason <54936265+danielfreyrg@users.noreply.github.com>
2021-11-30 14:44:49 +0000

    Update Contact.jsx

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity, Linking } from "react-native";
 import { color } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 import styles from "./styles";
 
@@ -15,8 +15,8 @@ export default function Contact() {
       <Text style={styles.name}>Daníel Freyr Gylfason</Text>
       <View style={styles.mobile}>
           <Text style={styles.mobileheader}>mobile</Text>
-          <Text style={styles.phonenr}>866-0115</Text>
-          <TouchableOpacity style={styles.button} >
+          <Text style={styles.phonenr} onPress={()=>{Linking.openURL('tel://+3548660115');}}>866-0115</Text>
+          <TouchableOpacity style={styles.button} onPress={()=>{Linking.openURL('tel://+3548660115');}}>
           <Text>Call </Text>
           </TouchableOpacity>
       </View>
