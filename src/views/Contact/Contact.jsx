9e1ce34bd9fac0a5114bebf0a0c416c347c6f8ec
Daníel Freyr Gylfason <54936265+danielfreyrg@users.noreply.github.com>
2021-11-30 14:14:58 +0000

    contact started

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { color } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+import styles from "./styles";
+
 
 export default function Contact() {
-    return (
-        <View>
-            <Text>This is the Contact screen loaded from views/Contact using navigation </Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+
+      <Image style={styles.image}
+        source={ require("./l60Hf.png") }
+
+      />
+      <Text style={styles.name}>Daníel Freyr Gylfason</Text>
+      <View style={styles.mobile}>
+          <Text style={styles.mobileheader}>mobile</Text>
+          <Text style={styles.phonenr}>866-0115</Text>
+          <TouchableOpacity style={styles.button} >
+          <Text>Call </Text>
+          </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
---
+++ src/views/Contact/styles.js
@@ -0,0 +1,69 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+    container: {
+      // flex: 1,
+      // backgroundColor: 'red',
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 0,
+    },
+    image: {
+        marginTop: 70,
+      justifyContent: "flex-start",
+      height: 80,
+      width: 80,
+
+      borderRadius: 50,
+    },
+    mobile: {
+        marginTop: 100,
+        width: '90%',
+        height: 70,
+        margin: 10,
+        backgroundColor: 'white',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 5,
+        borderRadius: 10
+    },
+    name: {
+        marginTop: 20,
+        fontWeight: 'bold',
+        fontSize: 25,
+    },
+    phonenr: {
+      // fontWeight: 'bolder',
+      marginLeft: 15,
+      marginTop:9,
+      color: '#6C85DD',
+      fontSize: 15,
+  }
+  ,
+    mobileheader: {
+      fontWeight: 'bold',
+      marginLeft: 10,
+      marginTop:9,
+      // color: '#6C85DD',
+      // fontSize: 10,
+  },
+  button: {
+      height: 40,
+      width: 100,
+      marginLeft: 150,
+      marginTop: 300,
+    color: 'blue',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: '#7BD389'
+
+},
+  });
