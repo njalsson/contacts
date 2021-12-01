dd88bfb450348b55aaf1b31e4b8a5361d29e457f
njalsson <njalsson1337@gmail.com>
2021-12-01 04:10:47 +0000

    contact thing eeeeeee

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { Text, View, TouchableWithoutFeedback } from 'react-native';
+import { Text, View, Image,  TouchableWithoutFeedback } from 'react-native';
 import styles from './style';
-
+import thumbnail from './profile.jpeg';
 
 
 export default function ContactItem({name}) {
     return (
-      <View>
-        <Text> {name}  </Text>
-        <Text></Text>
+      <View style={styles.contactguy}>
+        <Text style={styles.namethingy}> {name}  </Text>
+        <Image source={require('./profile.jpeg')} style={styles.thumbnail} />
       </View>
 
           );
-}
+        }
--- src/components/ContactItem/style.js
+++ src/components/ContactItem/style.js
@@ -2,7 +2,19 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     contactguy: {
+        flexDirection:"row",
         height: 64,
         width: 640
+    },
+    thumbnail: {
+      height:64,
+      width: 64,
+      marginRight: 50,
+      paddingRight: 50,
+    },
+    namethingy:{
+      fontSize :32,
+      paddingRight: 240,
+      marginTop: 10,
     },
 });
